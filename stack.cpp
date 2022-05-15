fa498208d44a9fb36b3e3171bfd07871926cf910
Yuan Biju <yuankavumkal652@gmail.com>
2022-05-15 17:48:42 +0530

    changed from malloc to new;replaced member variable names

--- stack.cpp
+++ stack.cpp
@@ -3,40 +3,44 @@
 #include "stack.h"
 
 Stack::Stack(int _size){
-    msize = _size;
-    marr = (int*)malloc(_size*sizeof(int));
+    m_size = _size;
+    m_arr = new int[m_size];
+}
+
+Stack::~Stack(){
+    free(m_arr);
 }
 
 void Stack::push(int _num){
-    if(mtop == msize) {std::cout<<"Stack overflow"<<std::endl;}
+    if(m_top == m_size) {std::cout<<"Stack overflow"<<std::endl;}
 
     else {
-        mtop += 1;
-        marr[mtop] = _num;
+        m_top += 1;
+        m_arr[m_top] = _num;
     }
 }
 
 void Stack::pop(){
-    if(mtop == -1) {std::cout<<"Stack underflow"<<std::endl;}
+    if(m_top == -1) {std::cout<<"Stack underflow"<<std::endl;}
 
-    else {mtop -= 1;}
+    else {m_top -= 1;}
 }
 
 int Stack::peek(){
-    if(mtop == -1){
+    if(m_top == -1){
         std::cout<<"Stack empty"<<std::endl;
         return -1;
     }
 
-    else{std::cout<<marr[mtop]<<std::endl;}
+    else{std::cout<<m_arr[m_top]<<std::endl;}
 }
 
 void Stack::display(){
-    if(mtop == -1){std::cout<<"Stack empty"<<std::endl;}
+    if(m_top == -1){std::cout<<"Stack empty"<<std::endl;}
 
     else {
-        for(int i=0;i<=mtop;i++){
-            std::cout<<marr[i]<<" ";
+        for(int i=0;i<=m_top;i++){
+            std::cout<<m_arr[i]<<" ";
         }
         std::cout<<std::endl;
     }
--- stack.h
+++ stack.h
@@ -1,11 +1,12 @@
 class Stack
 {
 private:
-int mtop = -1;
-int msize;
-int* marr;
+int m_top = -1;
+int m_size;
+int* m_arr;
 public:
     Stack(int _size);
+    ~Stack();
     void push(int _num);
     void pop();
     int peek();
